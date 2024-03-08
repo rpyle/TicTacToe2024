74725c8c40705a271d3558ad76497579d88e3e60
JDRnotRDJ <162379776+JDRnotRDJ@users.noreply.github.com>
2024-03-08 21:57:47 +0000

    added Dancs.py
    added my own module

---
+++ Dancs.py
@@ -0,0 +1,21 @@
+team_name = 'Dancs'
+strategy_name = 'Winning'
+strategy_description = 'win'
+
+def print_board(board):
+  print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
+  print('-+-+-')
+  print(board[1][0]+'|'+board[1][1]+'|'+board[1][2])
+  print('-+-+-')
+  print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
+
+def Move (player, board, score):
+  r = 0
+  c = 0
+  while board[r][c] != ' ':
+    c = c + 1
+    if c > 2:
+      c = 0
+      r = r + 1
+  
+  return r, c
