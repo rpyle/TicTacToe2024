b92e9dfc672179f2730b29d43e47763ffe91c95a
Iancoolcat <162379907+Iancoolcat@users.noreply.github.com>
2024-03-12 21:06:38 +0000

    Updated Burnscoolcat, removed unnecessary code

--- Burnscoolcat.py
+++ Burnscoolcat.py
@@ -1,6 +1,6 @@
 team_name = 'Burnscoolcat'
 strategy_name = 'Right Triangle Method'
-strategy_description = 'Try and take the backwards L and win or take the diagnol and win'
+strategy_description = 'Try and take the backwards L and win or take the diagonal and win'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -10,22 +10,9 @@ def print_board(board):
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
 
-'''def move(player, board, score):
-  if board[1][1] == ' ':
-    trythis(board)
-
-
-def trythis(board):
-  r = 0
-  c = 0
-  while board[r][c] != ' ':
-    r = r + 1
-    c = c + 1
-    
-  return r, c'''
 
 def move(player, board, score):
-  print_board(board)
+  
   r = 0
   c = 0
   
