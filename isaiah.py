983858821b52885bab03de25fc62c22fb9662a15
Isaiah355036 <162369299+Isaiah355036@users.noreply.github.com>
2024-03-12 19:09:21 +0000

    Updated strategy.

--- isaiah.py
+++ isaiah.py
@@ -1,6 +1,7 @@
 team_name = 'isaiah'
-strategy_name = 'Next Open'
-strategy_description = 'Play the next open spot.'
+strategy_name = 'Super Weenie Hut Jrs'
+strategy_description = 'I love corners.'
+
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -10,13 +11,38 @@ def print_board(board):
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
 def move(player, board, score):
+  print_board(board)
   r = 0
   c = 0
-  print_board(board)
-  while board[r][c] != ' ':
-    c = c + 1
-    if c > 2:
+ 
+  while board[r][c] != '':
+    if board[2][2] == ' ':
+      r = 2
+      c = 2
+    elif board[2][0] == ' ':
+      r = 2
       c = 0
-      r = r + 1
+    elif board[0][2] == ' ':
+      r = 0
+      c = 2
+    elif board[0][0] == ' ':
+      r = 0
+      c = 0
+    elif board[1][1] == ' ':
+      r = 1
+      c = 1
+    elif board[1][2] == ' ':
+      r = 1
+      c = 2
+    elif board[2][1] == ' ':
+      r = 2
+      c = 1
+    elif board[1][0] == ' ':
+      r = 1
+      c = 0
+    elif board[0][1] == ' ':
+      r = 0
+      c = 1
+    
+    return r, c
   
-  return r, c
