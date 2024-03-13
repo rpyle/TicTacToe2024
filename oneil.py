73d0bb5ee9728cd7ab304b0e5a8243524c8d704f
oneilcruz <162369327+oneilcruz@users.noreply.github.com>
2024-03-13 18:28:12 +0000

    finished oneil bot

--- oneil.py
+++ oneil.py
@@ -1,6 +1,6 @@
 team_name = 'oneil'
-strategy_name = ' '
-strategy_description = ' '
+strategy_name = 'four corners'
+strategy_description = 'attempts to claim the middle first, after that it plays in all four corners before filling in the remaing spaces'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -10,23 +10,39 @@ def print_board(board):
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
 def move(player, board, score):
-  print_board(board)
   r = 1
   c = 1
   if board[1][1] == ' ':
     r = 1
     c = 1
-  elif board[0][2] != ' ':
+  elif board[0][0] == ' ':
+    r = 0
+    c = 0
+  elif board[0][2] == ' ':
     r = 0
     c = 2
-  elif board[2][0] != ' ':
+  elif board[2][0] == ' ':
     r = 2
     c = 0
-  elif board[1][2] != ' ':
-    r = 1
+  elif board[2][2] == ' ':
+    r = 2
     c = 2
-  elif board[2][1] != ' ':
+  elif board[0][1] == ' ':
+    r = 0
+    c = 1
+  elif board[2][1] == ' ':
     r = 2
     c = 1
+  elif board[1][2] == ' ':
+    r = 1
+    c = 2
+  elif board[1][0] == ' ':
+    r = 1
+    c = 0
+
+
+
+
 
   return r, c
+ 
