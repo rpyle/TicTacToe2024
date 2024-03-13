07b867061b81b391e583adb2d4360c2d19b1fd44
CarterVoight <162369291+CarterVoight@users.noreply.github.com>
2024-03-13 06:07:50 +0000

    functioning strategy

--- carter.py
+++ carter.py
@@ -1,6 +1,6 @@
-team_name = 'E4'
-strategy_name = 'idk'
-strategy_description = 'idk'
+team_name = 'carter'
+strategy_name = 'Block'
+strategy_description = 'Block 3 of a kind if possible, if not then next open spot'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -12,59 +12,69 @@ def print_board(board):
 def move(player, board, score):
   r = 0
   c = 0
-  print(board)
-  '''  
-  while board[r][c] != ' ':
-    c = c + 1
-    if c > 2:
-      c = 0
-      r = r + 1
-      '''
-  if board[0][0] == 'X' and board[0][1] == 'X' or board[0][1] == 'X' and board[0][2] == 'X' or board[0][0] == 'X' and board[0][2] == 'X':
+
+  if board[0][0] != '' and board[0][1] != '' or board[0][1] != '' and board[0][2] != '' or board[0][0] != '' and board[0][2] != ''and not board[0][0] != '' and board[0][1] != '' and board[0][2] != '':
     while board[r][c] != ' ':
       c = c + 1
       if c > 2:
         c = 0
         r = r + 1
-        '''
-      return r, c
-      '''
-  elif board[1][0] == 'X' and board[1][1] == 'X' or board[1][1] == 'X' and board[1][2] == 'X' or board[1][0] == 'X' and board[1][2] == 'X':
+          
+        
+  elif board[1][0] != ' ' and board[1][1] != '' or board[1][1] != '' and board[1][2] != '' or board[1][0] != '' and board[1][2] != '' and not board[1][0] != '' and board[1][1] != '' and board[1][2] != '':
+
+    r = 1
+
     while board[r][c] != ' ':
+      c = c + 1
+      if c > 2:
+        c = 0
+        r = r + 1
+        
+       
+      
+  elif board[2][0] != '' and board[2][1] != '' or board[2][1] != '' and board[2][2] != '' or board[2][0] != '' and board[2][2] != '' and not board[2][0] != '' and board[2][1] != '' and board[2][2] != '':
+
+    r = 2
+
+    while board[r][c] != ' ':
+      c = c + 1
+      if c > 2:
+        c = 0
+        r = r + 1
+        
+  elif board[0][0] != '' and board[2][2] != '' or board[0][0] != '' and board[1][1] != '' or board[1][1] != '' and board[2][2] != '' and not board[0][0] != '' and board[1][1] != '' and board[2][2] != '':
+    if  board[0][0] != '' and board[2][2] != '':
       r = 1
-      c = c + 1
-      if c > 2:
-        c = 0
+      c = 1
+    elif board[0][0] != '' and board[1][1] != '' or board[1][1] != '' and board[2][2] != '':
+      c = 0
+      r = 0
+      while board[r][c] != ' ':
+        r = r - 1
+        if r < 0:
+          r = 2
+          c = c + 2
+       
+  elif board[0][2] != '' and board[1][1] != '' or board[0][2] != '' and board[2][0] != '' or board[1][1] != '' and board[2][0] != '' and not board[0][2] != '' and board[0][1] != '' and board[2][0] != '':
+    if  board[0][2] != '' and board[2][0] != '':
+      r = 1
+      c = 1
+    elif board[0][2] != '' and board[1][1] != '' or board[1][1] != '' and board[2][0] != '':
+      c = 2
+      r = 0
+      while board[r][c] != ' ':
         r = r + 1
-      return r, c
-  elif board[2][0] == 'X' and board[2][1] == 'X' or board[2][1] == 'X' and board[2][2] == 'X' or board[2][0] == 'X' and board[2][2] == 'X':
-    while board[r][c] != ' ':
-      r = 2
-      c = c + 1
-      if c > 2:
-        c = 0
-        r = r + 1
-      return r, c
-  elif board[0][0] == 'X' and board[2][2] == 'X' or board[0][0] == 'X' and board[1][1] == 'X' or board[1][1] == 'X' and board[2][2] == 'X':
-    while board[r][c] != ' ':
-      r = r + 1
-      c = c + 1
-      if c > 2:
-        c = 0
-        r = r + 1
-      return r, c
-  elif board[0][2] == 'X' and board[1][1] == 'X' or board[0][2] == 'X' and board[2][0] == 'X' or board[1][1] == 'X' and board[2][0] == 'X':
-    while board[r][c] != ' ':
-      r = r + 1
-      c = c + 1
-      if c > 2:
-        c = 0
-        r = r + 1
-      return r, c
+        if r > 2:
+          r = 0
+          c = c + 1
+          
+      
   else:
     while board[r][c] != ' ':
       c = c + 1
       if c > 2:
         c = 0
         r = r + 1
+        
   return r, c
