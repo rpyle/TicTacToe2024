3981ae43cac485da71a225caa5ab2c0b06812da1
354859 <162369240+354859@users.noreply.github.com>
2024-03-13 05:41:04 +0000

    kburcham strategy finished

--- kburcham.py
+++ kburcham.py
@@ -1,6 +1,6 @@
 team_name = 'kburcham'
-strategy_name = ''
-strategy_description = ''
+strategy_name = 'Two corners'
+strategy_description = 'Pick the middle if possible and use two corners to have two options to win.'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -10,24 +10,47 @@ def print_board(board):
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
 def move(player, board, score):
-  print_board(board)
-  r = 0
-  c = 0
+  moves = 0
+  r = 1
+  c = 1
   while board[r][c] != ' ':
     if board[r][c] == player:
-      r = 2
-      c = 2
-    else:
-      r = 2
-    if board[r][c] != ' ':
+      if board[0][2] == " ":
+        r = 0
+        c = 2
+        if board[2][0] == " ":
+          r = 2
+          c = 0
       if board[r][c] == player:
-        r = r + 2
-        c = c + 2
-      r = 0
-      c = 2
-    if board[r][c] != ' ':
-      r = 2
-      c = 0
-
-  print(r,c)
+        r = 2
+        c = 2
+      else:
+        if board[0][2] != " ":
+          if board[0][2] != player:
+            r = 2
+            c = 1
+            if board[r][c] == ' ':
+              break
+          else:
+            r = 1
+            c = 2
+    else:
+      if board[r][c] != ' ':
+        if board[r][c] == player:
+          r = 0
+          c = 2
+        else:
+          r_list = [0,1,2]
+          if board[r_list[r] - 1][c] != player:
+            r = r_list[r-2]
+          else:
+            r = r_list[r-1]
+      if board[r][c] != ' ':
+        if board[r][c] == player:
+          r = 2
+          c = 0
+    moves += 1
+    if moves >= 9:
+        break
+  
   return r, c
