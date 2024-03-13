799ffa3c6a7290fbc6462805c63bd042b46d8b7e
koenkuder <162369248+koenkuder@users.noreply.github.com>
2024-03-13 05:45:37 +0000

    amazing new strat

--- koen.py
+++ koen.py
@@ -1,6 +1,6 @@
 team_name = 'koen'
-strategy_name = ''
-strategy_description = ''
+strategy_name = 'Middle & Corners'
+strategy_description = 'Play middle and corners, then play first open spots.' 
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -10,13 +10,28 @@ def print_board(board):
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
 def move(player, board, score):
-  r = 0
-  c = 0
   if board[1][1] == ' ':
-    return 2, 2
+    r = 1
+    c = 1
   elif board[0][0] == ' ':
-    return 0, 0
+    r = 0
+    c = 0
   elif board[0][2] == ' ':
-    return 0, 2
-  elif
+    r = 0
+    c = 2
+  elif board[2][0] == ' ':
+    r = 2
+    c = 0
+  elif board[2][2] == ' ':
+    r = 2
+    c = 2
+  else:
+    for r in range(3):
+      for c in range(3):
+          if board[r][c] == ' ':
+            return r, c
+  return r, c
+  
+    
+
     
--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import importlib
 import example0
 import example1
 import example2
-
-
+import koen
 
 number_of_rounds = 100
 modules = [example0,
            example1,
-           example2
+           example2,
+           koen
           ]
 
 test_board = [['O', 'X', 'O'],
