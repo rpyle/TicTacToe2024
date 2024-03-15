7ef25a7c664cde6366d4986d20814d193f3b4fd8
natalyahuys <162369261+natalyahuys@users.noreply.github.com>
2024-03-15 08:13:31 +0000

    natalya's strategy

--- natalya.py
+++ natalya.py
@@ -1,6 +1,6 @@
-team_name = 'natalya'
-strategy_name = 'Next Open'
-strategy_description = 'Play the next open spot.'
+team_name = 'Natalya'
+strategy_name = 'Natalya'
+strategy_description = 'Play the right column.'
 
 def print_board(board):
   print(board[0][0]+'|'+board[0][1]+'|'+board[0][2])
@@ -11,11 +11,10 @@ def print_board(board):
 
 def move(player, board, score):
   r = 0
-  c = 0
+  c = 2
   while board[r][c] != ' ':
-    c = c + 1
-    if c > 2:
-      c = 0
-      r = r + 1
-  
+    r = r + 1
+    if r > 2:
+      r = 0
+      c = c - 1
   return r, c
