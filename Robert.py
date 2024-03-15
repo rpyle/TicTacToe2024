31f88fc62dade320069a3368097258fd8c29fa1b
RobTigau <162379539+RobTigau@users.noreply.github.com>
2024-03-15 00:48:14 +0000

    Created a program that changes its starting moves based on what the first move of the game is.

--- Robert.py
+++ Robert.py
@@ -1,10 +1,8 @@
-import example0
-import example1
-import example2
-import Robert
 import random
+
+
 team_name = 'Rob'
-strategy_name = 'Immaginary Technique. Purple'
+strategy_name = 'Imaginary Technique. Hollow. Purple'
 strategy_description = 'Throughout heaven and earth, I alone am the honored one'
 
 def print_board(board):
@@ -14,64 +12,347 @@ def print_board(board):
   print('-+-+-')
   print(board[2][0]+'|'+board[2][1]+'|'+board[2][2])
 
-  
 
 def move(player, board, score):
+  r, c = 0, 0
   if player == 'O':
     if board[0][0] == 'X':
-      moveset1()
-    if board[0][1] == 'X':
-      moveset2()
-    if board[0][2] == 'X':
-      moveset3()
-    if board[1][0] == 'X':
-      moveset4()
-    if board[1][1] == 'X':
-      moveset5()
-    if board[1][2] == 'X':
-      moveset6()
-    if board[2][0] == 'X':
-      moveset7()
-    if board[2][1] == 'X':
-      moveset8()
-    if board[2][2] == 'X':
-      moveset9()
-  elif player == 'X'""
-  r = 0 
+      r, c = movesetstart2(player, board, score)
+    elif board[0][1] == 'X':
+      r, c = movesetstart2(player, board, score)
+    elif board[0][2] == 'X':
+      r, c = movesetstart2(player, board, score)
+    elif board[1][0] == 'X':
+      r, c = movesetstart2(player, board, score)
+    elif board[1][1] == 'X':
+      r, c = moveset5(player, board, score)
+    elif board[1][2] == 'X':
+      r, c = movesetstart2(player, board, score)
+    elif board[2][0] == 'X':
+      r, c = movesetstart2(player, board, score)
+    elif board[2][1] == 'X':
+      r, c = movesetstart2(player, board, score)
+    elif board[2][2] == 'X':
+      r, c = movesetstart2(player, board, score)
+  elif player == 'X':
+    r, c = movesetstart(player, board, score)
+  return r, c
+
+def movesetstart(player, board, score):
+  r = 0
   c = 0
-  while board[r][c] != ' ':
-    if player == 'X':
-      opp = 'O'
-    else:
-      opp = 'X'
+  moves = 1
+  while moves != 9:
       if board[1][1] == ' ':
-        return 1, 1
-      else:
-        if board[0][0] == ' ':
-          return 0, 0
-        elif board[0][2] == ' ':
-          return 0, 2
-        elif board[2][0] == ' ':
-          return 2, 0
-        elif board[2][2] == ' ':
-          return 2, 2
-      if board[0][2] == ' ':
-        return 0, 2
+        moves += 2
+        r, c = 1, 1
+      elif board[0][0] == ' ':
+        moves += 2
+        r, c = 0, 0
+      elif board[0][2] == ' ':
+        moves += 2
+        r, c = 0, 2
       elif board[2][0] == ' ':
-        return 2, 0
+        moves += 2
+        r, c = 2, 0
       elif board[2][2] == ' ':
-        return 2, 2
-      elif board[0][1] == ' ':
-        return 0, 1
-      elif board[1][0] == ' ':
-        return 1, 0
-      elif board[2][1] == ' ':
-        return 1, 0
-      elif board[1][2] == ' ':
-        return 1, 2
-      if board[0][0] == 'X' and board[0][2] == 'X':
-        return 0, 1
-      if board[0][0] == 'X' and board[2][0] == 'X':
-        return 1, 0
-        
+        moves += 2
+        r, c = 2, 2
+      elif board[0][0] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 1
+      elif board[0][0] == 'O' and board[0][1] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 2
+      elif board[0][1] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[0][0] == 'O' and board[2][0] == 'O' and moves == 5:
+        moves += 2
+        r, c = 1, 0
+      elif board[0][0] == 'O' and board[1][0] == 'O' and moves == 5:
+        moves += 2
+        r, c = 2, 0
+      elif board[1][0] == 'O' and board[2][0] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[2][0] == 'O' and board[2][1] == 'O' and moves == 5:
+        moves += 2
+        r, c = 2, 2
+      elif board[2][0] == 'O' and board[2][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 2, 1
+      elif board[2][1] == 'O' and board[2][2] == 'O' and moves == 5:
+        moves += 2 
+        r, c = 2, 1
+      elif board[2][2] == 'O' and board[1][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 2
+      elif board[1][2] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves +=2 
+        r, c = 2, 2
+      elif board[2][2] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 1, 2
+      elif board[0][0] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 1
+      elif board[0][0] == 'X' and board[0][1] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 2
+      elif board[0][1] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[0][0] == 'X' and board[2][0] == 'X' and moves == 5:
+        moves += 2
+        r, c = 1, 0
+      elif board[0][0] == 'X' and board[1][0] == 'X' and moves == 5:
+        moves += 2
+        r, c = 2, 0
+      elif board[1][0] == 'X' and board[2][0] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[2][0] == 'X' and board[2][1] == 'X' and moves == 5:
+        moves += 2
+        r, c = 2, 2
+      elif board[2][0] == 'X' and board[2][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 2, 1
+      elif board[2][1] == 'X' and board[2][2] == 'X' and moves == 5:
+        moves += 2 
+        r, c = 2, 1
+      elif board[2][2] == 'X' and board[1][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 2
+      elif board[1][2] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves +=2 
+        r, c = 2, 2
+      elif board[2][2] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 1, 2
+      else:
+        if board[r][c] == ' ' and moves == 7:
+          moves += 2
+          r, c = r, c
+        else:
+          moves += 2
+          r = random.randint(0, 2)
+          c = random.randint(0, 2)
+  return r, c 
+      
+      
+
+def movesetstart2(player, board, score):
+  r = 0
+  c = 0
+  moves = 1
+  while moves != 9:
+      if board[1][1] == ' ':
+        moves += 2
+        r, c = 1, 1
+      elif board[0][0] == ' ':
+        moves += 2
+        r, c = 0, 0
+      elif board[0][2] == ' ':
+        moves += 2
+        r, c = 0, 2
+      elif board[2][0] == ' ':
+        moves += 2
+        r, c = 2, 0
+      elif board[2][2] == ' ':
+        moves += 2
+        r, c = 2, 2
+      elif board[0][2] == ' ':
+        moves += 2
+        r, c = 0, 2
+      elif board[0][0] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 1
+      elif board[0][0] == 'O' and board[0][1] == 'O' and moves == 5:
+        moves += 2
+        r,c = 0, 2
+      elif board[0][1] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[0][0] == 'O' and board[2][0] == 'O' and moves == 5:
+        moves += 2
+        r, c = 1, 0
+      elif board[0][0] == 'O' and board[1][0] == 'O' and moves == 5:
+        moves += 2
+        r, c = 2, 0
+      elif board[1][0] == 'O' and board[2][0] == 'O' and moves == 5:
+        moves += 2
+        r,c = 0, 0
+      elif board[2][0] == 'O' and board[2][1] == 'O' and moves == 5:
+        moves += 2
+        r,c = 2, 2
+      elif board[2][0] == 'O' and board[2][2] == 'O' and moves == 5:
+        moves += 2
+        r,c = 2, 1
+      elif board[2][1] == 'O' and board[2][2] == 'O' and moves == 5:
+        moves += 2 
+        r, c = 2, 1
+      elif board[2][2] == 'O' and board[1][2] == 'O' and moves == 5:
+        moves += 2
+        r,c = 0, 2
+      elif board[1][2] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves +=2 
+        r,c = 2, 2
+      elif board[2][2] == 'O' and board[0][2] == 'O' and moves == 5:
+        moves += 2
+        r,c = 1, 2
+      elif board[0][0] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 1
+      elif board[0][0] == 'X' and board[0][1] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 2
+      elif board[0][1] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[0][0] == 'X' and board[2][0] == 'X' and moves == 5:
+        moves += 2
+        r, c = 1, 0
+      elif board[0][0] == 'X' and board[1][0] == 'X' and moves == 5:
+        moves += 2
+        r, c = 2, 0
+      elif board[1][0] == 'X' and board[2][0] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 0
+      elif board[2][0] == 'X' and board[2][1] == 'X' and moves == 5:
+        moves += 2
+        r, c = 2, 2
+      elif board[2][0] == 'X' and board[2][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 2, 1
+      elif board[2][1] == 'X' and board[2][2] == 'X' and moves == 5:
+        moves += 2 
+        r, c = 2, 1
+      elif board[2][2] == 'X' and board[1][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 0, 2
+      elif board[1][2] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves +=2 
+        r, c = 2, 2
+      elif board[2][2] == 'X' and board[0][2] == 'X' and moves == 5:
+        moves += 2
+        r, c = 1, 2
+      else:
+        if board[r][c] == ' ' and moves == 7:
+          moves += 2
+          r, c = r, c
+        else:
+          moves +=2
+          r = random.randint(0, 2)
+          c = random.randint(0, 2)
+  return r, c
+
+
+def moveset5(player, board, score):
+  r = 0
+  c = 0
+  moves = 1
+  while moves != 9:
+    if board[1][1] == 'X':
+      moves += 2
+      r, c = 0, 0
+    elif board[0][1] == 'X':
+      moves += 2
+      r, c = 2, 1
+    elif board[1][0] == 'X':
+      moves += 2
+      r, c = 1, 2
+    elif board[2][1] == 'X':
+      moves += 2
+      r, c = 0, 1
+    elif board[1][2] == 'X':
+      moves += 2
+      r, c = 1, 0
+    elif board[0][2] == 'X':
+      moves += 2
+      r,c = 2, 0
+    elif board[2][0] == 'X':
+      moves += 2
+      r, c = 0, 2
+    elif board[0][0] == 'O' and board[0][2] == 'O' and moves == 5:
+      moves += 2
+      r, c = 0, 1
+    elif board[0][0] == 'O' and board[0][1] == 'O' and moves == 5:
+      moves += 2
+      r,c = 0, 2
+    elif board[0][1] == 'O' and board[0][2] == 'O' and moves == 5:
+      moves += 2
+      r, c = 0, 0
+    elif board[0][0] == 'O' and board[2][0] == 'O' and moves == 5:
+      moves += 2
+      r, c = 1, 0
+    elif board[0][0] == 'O' and board[1][0] == 'O' and moves == 5:
+      moves += 2
+      r, c = 2, 0
+    elif board[1][0] == 'O' and board[2][0] == 'O' and moves == 5:
+      moves += 2
+      r,c = 0, 0
+    elif board[2][0] == 'O' and board[2][1] == 'O' and moves == 5:
+      moves += 2
+      r,c = 2, 2
+    elif board[2][0] == 'O' and board[2][2] == 'O' and moves == 5:
+      moves += 2
+      r,c = 2, 1
+    elif board[2][1] == 'O' and board[2][2] == 'O' and moves == 5:
+      moves += 2 
+      r, c = 2, 1
+    elif board[2][2] == 'O' and board[1][2] == 'O' and moves == 5:
+      moves += 2
+      r,c = 0, 2
+    elif board[1][2] == 'O' and board[0][2] == 'O' and moves == 5:
+      moves +=2 
+      r,c = 2, 2
+    elif board[2][2] == 'O' and board[0][2] == 'O' and moves == 5:
+      moves += 2
+      r,c = 1, 2
+    elif board[0][0] == 'X' and board[0][2] == 'X' and moves == 5:
+      moves += 2
+      r, c = 0, 1
+    elif board[0][0] == 'X' and board[0][1] == 'X' and moves == 5:
+      moves += 2
+      r, c = 0, 2
+    elif board[0][1] == 'X' and board[0][2] == 'X' and moves == 5:
+      moves += 2
+      r, c = 0, 0
+    elif board[0][0] == 'X' and board[2][0] == 'X' and moves == 5:
+      moves += 2
+      r, c = 1, 0
+    elif board[0][0] == 'X' and board[1][0] == 'X' and moves == 5:
+      moves += 2
+      r, c = 2, 0
+    elif board[1][0] == 'X' and board[2][0] == 'X' and moves == 5:
+      moves += 2
+      r, c = 0, 0
+    elif board[2][0] == 'X' and board[2][1] == 'X' and moves == 5:
+      moves += 2
+      r, c = 2, 2
+    elif board[2][0] == 'X' and board[2][2] == 'X' and moves == 5:
+      moves += 2
+      r, c = 2, 1
+    elif board[2][1] == 'X' and board[2][2] == 'X' and moves == 5:
+      moves += 2 
+      r, c = 2, 1
+    elif board[2][2] == 'X' and board[1][2] == 'X' and moves == 5:
+      moves += 2
+      r, c = 0, 2
+    elif board[1][2] == 'X' and board[0][2] == 'X' and moves == 5:
+      moves +=2 
+      r, c = 2, 2
+    elif board[2][2] == 'X' and board[0][2] == 'X' and moves == 5:
+      moves += 2
+      r, c = 1, 2
+    else:
+      if board[r][c] == ' ':
+        moves += 2
+        r, c = r, c
+      else:
+        moves += 2
+        r = random.randint(0, 2)
+        c = random.randint(0, 2)
+  return r, c
 
